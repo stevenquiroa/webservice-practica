7f060a54423c07d720c0fddbd5a287b00ad9dbf6
Steven Quiroa <stevenkm92@gmail.com>
2015-04-28 19:05:28 -0600

    Agregado usuarios connectados al app

--- client/app.js
+++ client/app.js
@@ -8,6 +8,7 @@ const id = uuid.v4()
 const buttons = document.querySelectorAll('.button')
 const left = document.querySelector('#left')
 const right = document.querySelector('#right')
+const con = document.querySelector('#connections')
 
 var l = 0, r = 0
 
@@ -18,6 +19,8 @@ for (var i = buttons.length - 1; i >= 0; i--) {
 socket.on('autoback', set_ponits)
 
 socket.on('resultback', set_ponits)
+
+socket.on('userconnect', set_connections)
 
 function action () {
 	const dir = this.id
@@ -33,4 +36,7 @@ function set_ponits (message) {
 	right.innerHTML = message.right
 	left.style.width = message.left + '%'
 	right.style.width = message.right + '%'
+}
+function set_connections (message) {
+	con.innerHTML = message.number
 }
--- realtime/index.js
+++ realtime/index.js
@@ -6,11 +6,18 @@ module.exports = function (server) {
 	const io = socketio(server)
 	let left = 25
 	let right = 25 
+	let connections = 0
 	io.on('connection', onConnection)
 
 	function onConnection (socket) {
 		console.log(`Client connected ${socket.id}`)
-		
+		let rleft = left * 2
+		let rright = right * 2		
+
+		socket.emit('init', {left: rleft, right: rright, number: connections++})
+		socket.broadcast.emit('userconnect', {number: connections++})
+		// socket.emit('userconnect', {})
+
 		socket.on('click', function (direction) {
 			if (direction.left) {
 				left += 1
@@ -26,6 +33,11 @@ module.exports = function (server) {
 			let message = {left: rleft, right : rright}
 			socket.broadcast.emit('resultback', message)
 			socket.emit('autoback', message)
+		})
+
+		socket.on('disconnect', function () {
+			socket.broadcast.emit('userconnect', {number: connections--})
+			console.log(`Client disconnected ${socket.id}`)
 		})
 	}
 }
